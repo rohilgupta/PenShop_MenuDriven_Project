2c6317c955f3a78f6f680f20b41d1ee71024b540
Rohil Gupta <75613163+rohilgupta@users.noreply.github.com>
2023-11-23 23:41:54 +0530

    Delete Desktop directory

--- Desktop/penShop/src/enums/PenColor.java
+++
@@ -1,5 +0,0 @@
-package enums;
-
-public enum PenColor {
-	BLACK, BLUE, RED, GREEN;
-}
--- Desktop/penShop/src/validations/PensException.java
+++
@@ -1,9 +0,0 @@
-package validations;
-
-public class PensException extends Exception{
-
-	public PensException(String err)
-	{
-		super(err);
-	}
-}
